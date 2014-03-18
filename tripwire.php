aab86e457723a7e460dcc77aa79da182f8b06ed0
Daniel Walker <polyesterhat@gmail.com>
2014-03-18 19:10:05 -0400

    revamped tripwire library
    * now a class structure
    * uses a message buffer instead of echo
    * can scan multiple paths
    
    Todo: finish the engine; using old and new code

--- tripwire.php
+++ tripwire.php
@@ -3,6 +3,7 @@
 /*
  *  Tripwire
  *  Luke Stevenson <www.lucanos.com>
+ *  and Daniel Walker <polyesterhat@gmail.com>
  *
  *  This is a PHP script which will scan files within a directory (including
  *    sub-directories), calculate their MD5 Hashes and then compare them against
@@ -18,352 +19,157 @@
  *
  */
 
-set_time_limit( 0 );
+class Tripwire
+{
+    protected $config;
 
-if( file_exists( 'tripwire_config.php' ) ){
+    /**
+     * a place to keep the messages before they all get echoed
+     * 
+     * @var array
+     */
+    protected $messages_buffer = array();
 
- # Standalone Config File (for easy upgrading of this file)
-  require_once( 'tripwire_config.php' );
+    public function __construct()
+    {
+        $this->load_settings('tripwire_config.ini');
 
-}else{
+        // make a debug shortcut
+        $this->DEBUG = $this->config['debug'];
 
- # Default Configuration
-  $config = array(
+        // start checking the supplied dirs
+        $this->check_paths();
 
-    // Debugging (Verbose Output)
-    'debug' => false ,
-
-    // File Containing the MD5 Keys
-    'md5file' => 'tripwire_filelist.md5' ,
-    // Delimited for Filelist
-    'delim' => '&&&' ,
-
-    'exclude' => array(
-      // Specific Files to Exclude from Scanning
-      'files' => array(
-        '.' ,
-        '..' ,
-        'tripwire_filelist.md5' ,
-        'tripwire_config.php' ,
-        'error_log' ,
-        'backup.zip' ,
-        '.bash_history'
-      ) ,
-      // Extensions to Exclude from Scanning
-      'extensions' => array(
-        'afm' ,
-        // Flash
-        'flv' , 'swf' ,
-        // Images
-        'bmp' , 'gif' , 'jpeg' , 'jpg' , 'png' , 'psd' ,
-        'log' ,
-        'txt' ,
-        // Videos
-        'mp4' , 'mov' , 'ogv' , 'webm' ,
-        // Dreamweaver
-        'mno' ,
-        // Audio
-        'aif' , 'mp3' ,
-        // Microsoft Word Files
-        'doc' , 'docx' , 'xls' , 'xlsx' ,
-        // Compressed Files
-        '7z' , 'rar' , 'sitx' , 'zip'
-      )
-    ) ,
-
-    'email' => array(
-      'to' => array(            // Email these people on changes
-        // 'user@server.com'
-      ) ,
-      'title' => '[Tripwire] Change Detected - [X] Files' , // The Email Title
-      'body' => "Tripwire (https://github.com/lucanos/Tripwire) has detected a number of changes:\n\n[AN] Files Added:\n[AF]\n\n[MN] Files Modified:\n[MF]\n\n[DN] Files Deleted:\n[DF]\n\n"   // The Email Template
-    )
-
-  );
-
-}
-
-
-
-define( 'DEBUG' , isset( $config['debug'] ) && $config['debug'] );
-
-
-function debugMsg( $msg = false ){
-  if( DEBUG && $msg )
-    echo $msg;
-}
-
-
-function run_tripwire( $dir=false ){
-  global $config, $filelist;
-
-  // If no Directory Specified, Default to the Root of the Site the script is executed under
-  if( !$dir )
-    $dir = $_SERVER['DOCUMENT_ROOT'];
-
-  // If last character is a slash, strip it off the end
-  if( substr( $dir , -1 )=='/' )
-    $dir = substr( $dir , 0 , -1 );
-
-  // If the supplied variable is not a Directory, terminate
-  if( !is_dir( $dir ) ){
-    debugMsg( "Directory '$dir' does not exist.\n" );
-    return false;
-  }
-  debugMsg( "Checking directory '$dir'\n" );
-
-  $temp = array();
-  $d = dir( $dir );
-
-  // Loop through the files
-  while( false!==( $entry = $d->read() ) ){
-  
-    // Full Entry (including Directory)
-    $entry_full = $dir.'/'.$entry;
-    
-    // Debugging
-    debugMsg( "<tr><th align='left'>$entry_full</th>" );
-      
-    // Symbolic Link - Excluded
-    if( is_link( $entry ) ){
-      debugMsg( "<td><em>Symbolic Link</em></td></tr>\n" );
-      continue;
+        var_dump($this->messages_buffer);
     }
 
-    // Excluded File/Folder
-    if( $f1 = in_array( $entry , $config['exclude']['files'] )
-        || in_array( $entry_full , $config['exclude']['files'] ) ){
-      debugMsg( "<td><em>Excluded File/Folder (".array_search( isset( $f1 ) && $f1 ? $entry : $entry_full , $config['exclude']['files'] ).")</em></td></tr>\n" );
-      continue;
+
+    /**
+     * this is the real meat and potatoes of Tripwire
+     * 1. first get paths from config file
+     * 2. traverse them and check for differences in the 
+     *    date modified to what we have on file
+     * 3.
+     * 
+     * @since   2014-03-17
+     * @author  Daniel.Walker <polyesterhat@gmail.com>
+     * @return  void
+     */
+    protected function check_paths()
+    {
+        $paths = $this->config['paths'];
+
+        if (!is_array($paths))
+        {
+            $this->add_message('In config, paths should be an array');
+            $paths = (array) $paths;
+        }
+
+        // If no Directory Specified, Default to the Root 
+        // of the Site the script is executed under
+        if (empty($paths))
+        {
+            $this->add_message('Paths is empty in the config - it shouldn\'t be');
+            $paths = (array) $_SERVER['DOCUMENT_ROOT'];
+        }
+
+        foreach ($paths as $index => $path)
+        {
+            // If last character is a slash, strip it off the end
+            if( substr( $path , -1 ) == '/' )
+            {
+                $path = substr( $path , 0 , -1 );
+            }
+
+            // If the supplied variable is not a Directory, terminate
+            if (!is_dir($path))
+            {
+                $this->add_message("Directory '{$path}' does not exist.");
+
+                // go to next path
+                continue;
+            }
+
+            $this->add_message("Checking directory '{$path}'");
+
+            $this->check_path($path);
+
+            return $temp;
+        }
     }
 
-    // Excluded File Extension
-    if( in_array( pathinfo( $entry , PATHINFO_EXTENSION ) , $config['exclude']['extensions'] ) ){
-      debugMsg( "<td><em>Excluded File Extension (".array_search( pathinfo( $entry , PATHINFO_EXTENSION ) , $config['exclude']['extensions'] ).")</em></td></tr>\n" );
-      continue;
+
+    /**
+     * check just one path from the config file
+     * this is a recursive function
+     *
+     * @since   2014-03-17
+     * @author  Daniel.Walker <polyesterhat@gmail.com>
+     * @param   string     $path the path to the directory
+     * @return  void
+     */
+    protected function check_path($path = '')
+    {
+
     }
 
-    if( is_dir( $dir.'/'.$entry ) ){
-      // Recurse
-      debugMsg( "<td>Directory - Recursing</td></tr>\n" );
-      $temp = array_merge( $temp , run_tripwire( $dir.'/'.$entry ) );
-    }else{
-      $md5 = @md5_file( $dir.'/'.$entry );
-      debugMsg( "<td>$md5</td></tr>\n" );
-      if( !$md5 ){
-        debugMsg( "<td><strong>Unreadable. Adding to Unreadable List.</strong></td></tr>\n" );
-        file_put_contents( 'tripwire_unreadable.txt' ,  "{$dir}/{$entry} - Unreadable\n" , FILE_APPEND );
-      }else{
-        $temp[$dir.'/'.$entry] = $md5;
-      }
+
+    /**
+     * to maintain a buffer of messages
+     * this method will simply push a new string onto 
+     * the messages array
+     *
+     * @since   2014-03-17
+     * @author  Daniel.Walker <polyesterhat@gmail.com>
+     * @param   string $message a new message
+     * @return  void
+     */
+    protected function add_message($message)
+    {
+        array_push($this->messages_buffer, $message);
     }
-  }
-
-  $d->close();
-
-  return $temp;
-
-}
 
 
-// In case file_put_contents does not exist
-if( !function_exists( 'file_put_contents' ) ){
-  function file_put_contents( $filename , $data ){
-    $f = @fopen( $filename , 'w' );
-    if( !$f )
-      return false;
-    $bytes = fwrite( $f , $data );
-    fclose( $f );
-    return $bytes;
-  }
-}
+    /**
+     * just send an email using the config settings
+     * this $this->config
+     *
+     * @since   2014-03-17
+     * @author  Daniel.Walker <polyesterhat@gmail.com>
+     * @param   string     $message the body of the email
+     * @return  boolean
+     */
+    protected function email_user($message)
+    {
+        // Prepare the recipients
+        $to = implode( ', ' , $this->config['email']['to'] );
 
-
-// Init the Last Check List
-$last = array();
-
-
-// Access the MD5 File List, if it exists
-if( file_exists( $config['md5file'] ) ){
-  $temp = file( $config['md5file'] , FILE_SKIP_EMPTY_LINES );
-
-  // Split the MD5 File List into Filename and MD5 Hash
-  foreach( $temp as $line ){
-    list( $filename , $md5hash ) = explode( $config['delim'] , str_replace( "\n" , '' , $line ) );
-    $last[$filename] = $md5hash;
-  }
-
-  unset( $temp );
-}
-
-
-
-// Format
-if( DEBUG ){
-?>
-<pre><?php var_dump( $config['exclude'] ); ?></pre>
-<table border="1" cellpadding="2" cellspacing="0">
-  <thead>
-    <tr>
-      <th>File</th>
-      <th>Result</th>
-    </tr>
-  </thead>
-  <tbody>
-<?php
-}
-
-
-// Init the This Check List
-$now = run_tripwire( '.' );
-
-
-
-if( DEBUG ){
-?>
-  </tbody>
-</table>
-<?php
-}
-
-
-// Perform Comparisons
-
-// New Files = Files in $now, but not in $last
-$new = array_diff( array_keys( $now ) , array_keys( $last ) );
-
-// Deleted Files = Files in $last, but not in $now
-$deleted = array_diff( array_keys( $last ) , array_keys( $now ) );
-
-// Changed Files = Files in $last and $now, but with Different MD5 Hashes
-$modified = array_diff_assoc( array_intersect_key( $last , $now ) , array_intersect_key( $now , $last ) );
-
-
-if( DEBUG ){
-
-  echo '<pre>';
-  echo "\$new\n";
-  var_dump( $new );
-  echo "\$deleted\n";
-  var_dump( $deleted );
-  echo "\$modified\n";
-  var_dump( $modified );
-  echo '</pre>';
-
-}
-
-
-if( !count( $last ) // If there was no file list
-    || ( count( $new ) || count( $deleted ) || count( $modified ) ) ){ // Or a change was detected
-
-  # Update the file list
-
-  // Init an Array for Lines
-  $log = array();
-
-  // Compress the MD5 Hash Array into lines
-  foreach( $now as $k => $v )
-    $log[] = "{$k}{$config['delim']}{$v}";
-
-  // Write to the File List
-  file_put_contents( $config['md5file'] , implode( "\n" , $log ) );
-
-}
-
-
-
-// If there was a Filelist from the last run to compare against, and changes have occurred
-
-  # Changes Detected
-
-// Prepare Report
-
-if( count( $last )
-    && ( count( $new ) || count( $deleted ) || count( $modified ) ) ){
-  # Changes Detected
-
-  // Prepare the placeholder details
-  $body_replacements = array(
-    '[AN]' => count( $new ) ,
-    '[AF]' => ( count( $new ) ? implode( "\n" , $new ) : 'No Files' ) ,
-    '[MN]' => count( $modified ) ,
-    '[MF]' => ( count( $modified ) ? implode( "\n" , array_keys( $modified ) ) : 'No Files' ) ,
-    '[DN]' => count( $deleted ) ,
-    '[DF]' => ( count( $deleted ) ? implode( "\n" , $deleted ) : 'No Files' ) ,
-  );
-  $title = str_replace( '[X]' , ( count( $new )+count( $deleted )+count( $modified ) ) , $config['email']['title'] );
-  
-  // Send Email Flag
-  $sendEmail = true;
-
-}elseif( count( $last ) ){
- # No Changes Detected
-
-  // Prepare the placeholder details
-  $body_replacements = array(
-    '[AN]' => 0 ,
-    '[AF]' => 'No Files' ,
-    '[MN]' => 0 ,
-    '[MF]' => 'No Files' ,
-    '[DN]' => 0 ,
-    '[DF]' => 'No Files' ,
-  );
-  $title = str_replace( '[X]' , 0 , $config['email']['title'] );
-  
-  // Send Email Flag
-  $sendEmail = false;
-
-}else{
- # First Run
-
-  // Prepare the placeholder details
-  $body_replacements = array(
-    '[AN]' => count( $new ) ,
-    '[AF]' => ( count( $new ) ? implode( "\n" , $new ) : 'No Files' ) ,
-    '[MN]' => 0 ,
-    '[MF]' => 'No Files' ,
-    '[DN]' => 0 ,
-    '[DF]' => 'No Files' ,
-  );
-  $title = str_replace( '[X]' , count( $new ) , $config['email']['title'] );
-  
-  // Adjust the Template
-  $config['email']['body'] = "Tripwire has made it's first pass of your files.\n\n".$config['email']['body'];
-  
-  // Send Email Flag
-  $sendEmail = true;
-
-}
-
-
-// Perform the Placeholder Substitutions within the Body
-$body = str_replace(
-          array_keys( $body_replacements ) ,
-          $body_replacements ,
-          $config['email']['body']
+        // Send it
+        $result = mail(
+            $this->config['email']['to'],
+            $this->config['title'],
+            $message
         );
 
-
-if( count( $config['email']['to'] ) ){
-
-  if( $sendEmail ){
-
-    // Prepare the recipients
-    $to = implode( ', ' , $config['email']['to'] );
-    // Send it
-    if( mail( $to , $title , $body ) ){
-      echo "Email Sent Successfully\n";
-    }else{
-      echo "Email Failed\n";
+        return $result;
     }
-    
-  }else{
-  
-    // No Email Needed
-  
-  }
-  
-}else{
-  // Just echo the result
-  echo '<pre>'.$body.'</pre>';
+
+
+    /**
+     * find config file and parse using built in in parser
+     * load the settings into class variable
+     *
+     * @since   2014-03-17
+     * @author  Daniel.Walker <polyesterhat@gmail.com>
+     * @param   string $file_name the name of the config file for tripwire
+     * @return  void
+     */
+    protected function load_settings($file_name)
+    {
+        $this->config = parse_ini_file($file_name);
+    }
 }
+
+// Start the Instance
+$t = new Tripwire();
+
